1c9a69d6860ec4b36ef0f49a34398003dc564e71
Falcury <falcury@gmail.com>
2017-01-22 15:21:13 +0100

    Don't allow quickloading while recording a replay

--- src/seg000.c
+++ src/seg000.c
@@ -601,6 +601,12 @@ int __pascal far process_key() {
 		break;
 		case SDL_SCANCODE_F9:
 		case SDL_SCANCODE_F9 | WITH_SHIFT:
+#ifdef USE_REPLAY
+			if (recording) {
+				answer_text = "NO QUICKLOAD"; // quickloading would mess up the replay!
+				need_show_text = 1;
+			} else
+#endif
 			need_quick_load = 1;
 		break;
 #ifdef USE_REPLAY
