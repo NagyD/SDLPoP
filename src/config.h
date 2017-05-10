96a5020a21d150ff5e9d8a19e493987f06fc42d4
Falcury <falcury@gmail.com>
2017-05-10 11:00:07 +0200

    Tweaked the automatic input mode detection
    * Fixed not being able to start the game from the title screen by pressing shift (it unfortunately seems impossible to distinguish whether pressing shift was meant as control input, or as part of a hotkey, so we have to assume that it was meant as control...)
    * Check that the controller axis value is outside the dead zone to prevent accidental input being registered.
    * Changed the switching to be immediately processed and made the switching silent. (Not sure if this is the right choice, but it feels very seamless)

--- src/config.h
+++ src/config.h
@@ -33,6 +33,8 @@ The authors of this program may be contacted at http://forum.princed.org
 #define SDLPOP_VERSION "1.17"
 #define WINDOW_TITLE "Prince of Persia (SDLPoP) v" SDLPOP_VERSION
 
+#define JOY_THRESHOLD 8000
+
 // Enable or disable fading.
 // Fading used to be very buggy, but now it works correctly.
 #define USE_FADE
