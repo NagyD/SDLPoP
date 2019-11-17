8ab7f81642daefae7f06e8f3cfa04054b9889e36
b9 <hackerb9@member.fsf.org>
2019-11-16 18:36:08 -0800

    Revert "Disable the Enhanced QuickSave by default."
    This reverts commit 33a076727141ed8582dac99e5615a866094a61b3.

--- src/options.c
+++ src/options.c
@@ -424,7 +424,7 @@ void set_options_to_default() {
 	enable_controller_rumble = 1;
 	joystick_only_horizontal = 1;
 	joystick_threshold = 8000;
-	enable_quicksave = 0;
+	enable_quicksave = 1;
 	enable_quicksave_penalty = 1;
 	enable_replay = 1;
 	enable_lighting = 0;
