bd2b5ec731e54b97667767b6c302aa23492fa373
NagyD <nagydavid91@gmail.com>
2020-07-26 19:48:17 +0200

    Associate replay files (*.p1r) with SDLPoP in `make install`.

--- src/install.sh
+++ src/install.sh
@@ -3,5 +3,9 @@
 cd ..
 # Since we don't copy the executable or the data folder anywhere, the desktop file has to be updated to contain the actual paths.
 sed -e 's|$ROOT|'"$PWD"'|' src/SDLPoP.desktop.template > src/SDLPoP.desktop
-cp src/SDLPoP.desktop /usr/share/applications/
+cp src/SDLPoP.desktop ~/.local/share/applications/
 
+# Associate replay files (*.p1r) with SDLPoP.
+xdg-mime install src/user-extension-p1r.xml
+xdg-icon-resource install --context mimetypes --size 32 data/icon.png application-x-extension-p1r
+xdg-mime default ~/.local/share/applications/SDLPoP.desktop application/x-extension-p1r
--- src/uninstall.sh
+++ src/uninstall.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-rm /usr/share/applications/SDLPoP.desktop
+rm ~/.local/share/applications/SDLPoP.desktop
 
+# Deassociate replay files (*.p1r) from SDLPoP.
+xdg-mime uninstall user-extension-p1r.xml
+xdg-icon-resource uninstall --context mimetypes --size 32 application-x-extension-p1r
