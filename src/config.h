3481b7eac11c1f33a6b90b40acd5b85d8b23c56a
NagyD <nagydavid91@gmail.com>
2020-08-21 14:31:51 +0200

    Updated the version number where I forgot to.

--- src/config.h
+++ src/config.h
@@ -30,7 +30,7 @@ The authors of this program may be contacted at https://forum.princed.org
 #define POP_MAX_PATH 256
 #define POP_MAX_OPTIONS_SIZE 256
 
-#define SDLPOP_VERSION "1.20"
+#define SDLPOP_VERSION "1.21"
 #define WINDOW_TITLE "Prince of Persia (SDLPoP) v" SDLPOP_VERSION
 
 // Enable or disable the SDL hardware accelerated renderer backend
