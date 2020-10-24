307abbc68817540b0912fbc8075334b4a218eb82
NagyD <nagydavid91@gmail.com>
2020-10-24 11:32:11 +0200

    FIXED: Ctrl+S didn't mute music (except death music).

--- src/midi.c
+++ src/midi.c
@@ -515,7 +515,7 @@ void midi_callback(void *userdata, Uint8 *stream, int len) {
 			advance_us = advance_frames * ONE_SECOND_IN_US / mixing_freq; // recalculate, in case the rounding up increased this.
 			short* temp_buffer = malloc(advance_frames * 4);
 			OPL3_GenerateStream(&opl_chip, temp_buffer, advance_frames);
-			if (enable_music) {
+			if (is_sound_on && enable_music) {
 				for (int sample = 0; sample < advance_frames * 2; ++sample) {
 					((short*)stream)[sample] += temp_buffer[sample];
 				}
